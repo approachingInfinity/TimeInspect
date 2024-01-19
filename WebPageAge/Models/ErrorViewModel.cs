7f0fea6fb33a3b3dbb3cb9e17f059e096b21e71d
death_wish <drjcreation100@gmail.com>
2024-01-19 12:06:19 +0530

    add TimeInspect project

---
+++ WebPageAge/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebPageAge.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
